23efb5f7ccdf35ff5e993e17587a9cc014a1ff63
alejandro londono <alondono11@gmail.com>
2016-02-02 17:02:16 -0500

    adding counting object to boolean variable

--- sherlock.js
+++ sherlock.js
@@ -1,31 +1,38 @@
 function sherlock(n){
 	// while constraints have not been met, increment counter
 	var bool = [];
-	var newArray = 0;
-	function boolCount(newArray, bool){
-		if(boolean === true){
-			newArray[0]++;
-		}else{
-			newArray[1]++;
-		}
-		return newArray;
-	}
+	var boolCounts = {};
+	boolCounts.five = 0;
+	boolCounts.three= 0;
 	for (var i=0; i<n;i++){
 		bool.push(true);
 	}
-	// console.log(bool);
+	
 	var count = 1;
-	// console.log(bool);
-	while(count <= 8){
+	console.log(bool);
+	while(count <= 100){
 		for(var j=0;j<bool.length;j++){
+			if(bool[j] ===true){
+				boolCounts.five++;
+			}else{
+				boolCounts.three++;
+			}
+			
 			if(count % Math.pow(2,(j)) === 0){
 				bool[j] = !bool[j];
+
 			}
 		}
+		console.log(boolCounts);
+		if (boolCounts.five % 3 === 0 && boolCounts.three % 5 ===0){
+			console.log('DING DING DING');
+		}
+		boolCounts.three = 0;
+		boolCounts.five = 0;
 		console.log(bool);
-		bool.filter(boolCount(newArray, boolean));
+
 		
 		count++;
 	}
 }
-sherlock(4);
+sherlock(5);
