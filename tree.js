6d8e36ca8671a807bd72effee5e51c9548ceb177
alejandro londono <alondono11@gmail.com>
2016-02-04 17:05:21 -0500

    adding code which creates array of zeros

--- tree.js
+++ tree.js
@@ -1,16 +1,12 @@
 function growth(n){
 	var height = 1;
-	if(n === 0){
-		height = 1;
-	}else if(n % 2 === 0){
-		height = Math.pow(2,(n/2))+(n/2);
-	}else{
-		n--;
-		height = Math.pow(2,(n/2))+(n/2+1);
-	}
+
 	console.log(height);
+	var a = Array.apply(null, new Array(n)).map(function(){return 0});
+	console.log(a);
 }
 growth(1);
 growth(2);
 growth(3);
 // I need to create some sort of loop
+
